20f1605a9faa5ed9419346bb18b307564421de6c
DasChaos <non@example.com>
2019-05-15 21:54:01 +0200

    Added VS Task Runner Event for client

---
+++ Client/postBuild.bat
@@ -0,0 +1,11 @@
+﻿@echo off
+
+set outDirClient=..\output\client\
+
+rd /s /q %outDirClient%
+md %outDirClient%
+xcopy .output\* %outDirClient% /s /y /EXCLUDE:exclude.cfg
+xcopy webview\* %outDirClient%webview\* /s /y /EXCLUDE:exclude.cfg
+xcopy resource.cfg %outDirClient% /s /y /EXCLUDE:exclude.cfg
+xcopy index.mjs %outDirClient% /s /y /EXCLUDE:exclude.cfg
+rd /s /q xcopy .output\
